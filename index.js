7baf783ce6089c1564e5d380c24c5e99f1efd201
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-06-24 22:03:27 +0530

    deploying on render.com

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
-const serverless = require("serverless-http");
 
 const app = express();
 const mainRoute = require("./routes/mainRoute")
@@ -12,18 +11,7 @@ app.use(cors())
 app.use('/api/user', mainRoute)
 
 
-
-
-exports.handler = serverless(app);
-
-// Connect to the database
-dbConnect().then(() => {
-  console.log('Connected to the database');
-}).catch((error) => {
-  console.error('Error connecting to the database:', error);
-});
-
-// app.listen(5000,()=>{
-//     console.log('server is running on port 5000')
-//     dbConnect();
-// })
+app.listen(5000,()=>{
+    console.log('server is running on port 5000')
+    dbConnect();
+})
