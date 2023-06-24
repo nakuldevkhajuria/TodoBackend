a8e31829858e06752fbe684a18b1990ad40fb639
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-06-24 21:49:33 +0530

    added serverless-http package

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
+const serverless = require("serverless-http");
+
 const app = express();
 const mainRoute = require("./routes/mainRoute")
 const cors = require("cors")
+
+
 app.use(express.json())
 app.use(cors())
 app.use('/api/user', mainRoute)
 
 
-app.listen(5000,()=>{
-    console.log('server is running on port 5000')
-    dbConnect();
-})
+
+
+exports.handler = serverless(app);
+
+// Connect to the database
+dbConnect().then(() => {
+  console.log('Connected to the database');
+}).catch((error) => {
+  console.error('Error connecting to the database:', error);
+});
+
+// app.listen(5000,()=>{
+//     console.log('server is running on port 5000')
+//     dbConnect();
+// })
