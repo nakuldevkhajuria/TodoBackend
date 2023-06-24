fea6232fa820ca644fd896675904e6a0d1918b1c
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-06-24 18:30:23 +0530

    done with backend

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { createUser } = require("../controllers/UserCtrl");
+const { createUser, loginUser, forgetPassword } = require("../controllers/UserCtrl");
 const router = express.Router();
 
 router.get('/',(req,res)=>{
     res.send('hi htis is home')
 })
 
-router.post('/',createUser)
+router.post('/register',createUser)
+router.post("/login",loginUser)
+router.put("/:mobile",forgetPassword)
 
 module.exports = router
